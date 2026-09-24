cfbc1001f3f1ab4b4f222f35213fbb6de0cec7d3
ΛLΞX JΛMΞS ᗪEV <md.amirulislam8504@gmail.com>
2026-09-24 20:37:39 +0600

    Add files via upload

---
+++ config/app.ts
@@ -0,0 +1,4 @@
+/** Single source of truth for the web app's release/update identity. */
+export const APP_CURRENT_VERSION = '1.4.2';
+export const APP_UPDATE_REPOSITORY = 'AlexJamesHQ/Orion-Store';
+export const APP_UPDATE_REPOSITORY_URL = `https://github.com/${APP_UPDATE_REPOSITORY}`;
